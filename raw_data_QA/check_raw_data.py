cf97b203c2e24b1fb49b3bc0f6f03e2e303c2d16
tashwoods <tashwoods@gmail.com>
2020-05-11 17:21:52 -0500

    making arguments general

--- raw_data_QA/check_raw_data.py
+++ raw_data_QA/check_raw_data.py
@@ -4,20 +4,20 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 from datetime import datetime
 import calendar
+import argparse
 
-def aggregate_df(df):
-  df['cases'] = pd.to_numeric(df['cases'])
-  df['deaths'] = pd.to_numeric(df['deaths'])
-  aggregation_functions = {'cases':'sum', 'deaths':'sum', 'Date':'first'}
-  df_new = df.groupby(df['date']).aggregate(aggregation_functions)
-  df_new['new_cases'] = df_new['cases'].diff().fillna(0)
-  df_new['new_deaths'] = df_new['deaths'].diff().fillna(0)
+def aggregate_df(df, args):
+  df[args.new_cases_name] = pd.to_numeric(df[args.new_cases_name])
+  df[args.new_deaths_name] = pd.to_numeric(df[args.new_deaths_name])
+  aggregation_functions = {args.new_cases_name:'sum', args.new_deaths_name:'sum', args.updated_date_name:'first'}
+  df_new = df.groupby(df[args.date_name]).aggregate(aggregation_functions)
+  df_new['new_cases'] = df_new[args.new_cases_name].diff().fillna(0)
+  df_new['new_deaths'] = df_new[args.new_deaths_name].diff().fillna(0)
   return df_new
 
-def get_state(df, state):
-  df_new = df.loc[df['state'] == state]
-  df_new['Date'] = df[['date']]
-
+def get_state(df, state, args):
+  df_new = df.loc[df[args.state_name] == state]
+  df_new[args.updated_date_name] = df[[args.date_name]]
   return df_new
 
 def get_rmse(df1, df2, var):
@@ -26,15 +26,15 @@ def get_rmse(df1, df2, var):
   rmse = np.sqrt(mean_squared_error(df1[[var]], truncated_df2[[var]]))
   return round(rmse,2)
 
-def make_plot(var, df1, df2, df3, df1_name, df2_name, df3_name):
+def make_plot(var, df1, df2, df3, df1_name, df2_name, df3_name, args):
   rmse2 = get_rmse(df1, df3, var)
   rmse3 = get_rmse(df2, df3, var)
   plt.title(state)
   plt.xlabel(var)
-  plt.ylabel('Date')
-  plt.plot(df1['Date'], df1[var], color = 'blue', label = df1_name + ' NYT, RMSE: ' + str(rmse2), markersize = 8, marker = '.', alpha = 0.5)
-  plt.plot(df2['Date'], df2[var],  color = 'orange', label = df2_name + ' NYT, RMSE: ' + str(rmse3), markersize = 8, marker = '.', alpha = 0.5)
-  plt.plot(df3['Date'], df3[var], alpha = 0.5, color = 'green', label = df3_name + ' NYT: RMSE: ' + str(rmse3), markersize = 10, marker = '.')
+  plt.ylabel(args.updated_date_name)
+  plt.plot(df1[args.updated_date_name], df1[var], color = 'blue', label = df1_name + ' NYT, RMSE: ' + str(rmse2), markersize = 8, marker = '.', alpha = 0.5)
+  plt.plot(df2[args.updated_date_name], df2[var],  color = 'orange', label = df2_name + ' NYT, RMSE: ' + str(rmse3), markersize = 8, marker = '.', alpha = 0.5)
+  plt.plot(df3[args.updated_date_name], df3[var], alpha = 0.5, color = 'green', label = df3_name + ' NYT: RMSE: ' + str(rmse3), markersize = 10, marker = '.')
   plt.xticks(rotation=30)
   plt.legend(loc = 'upper left')
   plt.grid(True)
@@ -48,20 +48,31 @@ def get_current_day():
 
 
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description = 'arg parser for process.py')
+  #Output Dir------
+  parser.add_argument('-output_dir', '--output_dir', type = str, dest = 'output_dir', default = 'output', help = 'output_dir')
+  parser.add_argument('-state', '--state', type = list, dest = 'state', default = ['Idaho'], help = 'name of state to process')
+  parser.add_argument('-state_name', '--state_name', type = str, dest = 'state_name', default = 'state', help = 'name of state variable in input csv')
+  parser.add_argument('-new_cases_name', '--new_cases_name', type = str, dest = 'new_cases_name', default = 'cases', help = 'name of new cases column in input csv')
+  parser.add_argument('-new_deaths_name', '--new_deaths_name', type = str, dest = 'new_deaths_name', default = 'deaths', help = 'name of new deaths name in input csv')
+  parser.add_argument('-date_name', '--date_name', type = str, dest = 'date_name', default = 'date', help = 'name of datesin input csv')
+  parser.add_argument('-updated_date_name', '--updated_date_name', type = str, dest = 'updated_date_name', default = 'Date', help = 'name of date variable that is computed in this script, really just renaming')
   state = 'Idaho'
-  df1 = get_state(pd.read_csv("data/us-counties-may7.csv", parse_dates=['date']), state)
-  df2 = get_state(pd.read_csv("data/us-counties-may10.csv", parse_dates=['date']), state)
-  df3 = get_state(pd.read_csv("data/us-counties-latest.csv", parse_dates=['date']), state)
+  args = parser.parse_args()
+
+  df1 = get_state(pd.read_csv("data/us-counties-may7.csv", parse_dates=[args.date_name]), state, args)
+  df2 = get_state(pd.read_csv("data/us-counties-may10.csv", parse_dates=[args.date_name]), state, args)
+  df3 = get_state(pd.read_csv("data/us-counties-latest.csv", parse_dates=[args.date_name]), state, args)
   df1_name = 'May 7'
   df2_name = 'May 10'
   df3_name = get_current_day()
 
   #pd.set_option('display.max_rows', None)
 
-  df1_ag = aggregate_df(df1)
-  df2_ag = aggregate_df(df2)
-  df3_ag = aggregate_df(df3)
-  make_plot('new_cases', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name)
-  make_plot('new_deaths', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name)
+  df1_ag = aggregate_df(df1, args)
+  df2_ag = aggregate_df(df2, args)
+  df3_ag = aggregate_df(df3, args)
+  make_plot('new_cases', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name, args)
+  make_plot('new_deaths', df1_ag, df2_ag, df3_ag, df1_name, df2_name, df3_name, args)
 
 
