51577b7403829ddb8cd72f6f7db52fe0022bfce4
tashwoods <tashwoods@gmail.com>
2020-05-11 17:04:07 -0500

    cleaned up formatting of rmse and accessing data

--- raw_data_QA/check_raw_data.py
+++ raw_data_QA/check_raw_data.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import mean_squared_error
-
+from datetime import datetime
+import calendar
 
 def aggregate_df(df):
   df['cases'] = pd.to_numeric(df['cases'])
@@ -26,28 +27,35 @@ def get_rmse(df1, df2, var):
   return round(rmse,2)
 
 def make_plot(var, df1, df2, df3, df1_name, df2_name, df3_name):
-  rmse = get_rmse(df1, df2, var)
+  rmse2 = get_rmse(df1, df3, var)
+  rmse3 = get_rmse(df2, df3, var)
   plt.title(state)
   plt.xlabel(var)
   plt.ylabel('Date')
-  plt.plot(df1['Date'], df1[var], color = 'blue', label = df1_name + ' NYT', markersize = 8, marker = '.', alpha = 0.5)
-  plt.plot(df2['Date'], df2[var],  color = 'orange', label = df2_name + ' NYT: RMSE: ' + str(rmse), markersize = 8, marker = '.', alpha = 0.5)
-  #plt.plot(df3['Date'], df3[var], alpha = 0.5, color = 'green', label = df3_name + ' NYT', markersize = 10, marker = '.')
+  plt.plot(df1['Date'], df1[var], color = 'blue', label = df1_name + ' NYT, RMSE: ' + str(rmse2), markersize = 8, marker = '.', alpha = 0.5)
+  plt.plot(df2['Date'], df2[var],  color = 'orange', label = df2_name + ' NYT, RMSE: ' + str(rmse3), markersize = 8, marker = '.', alpha = 0.5)
+  plt.plot(df3['Date'], df3[var], alpha = 0.5, color = 'green', label = df3_name + ' NYT: RMSE: ' + str(rmse3), markersize = 10, marker = '.')
   plt.xticks(rotation=30)
   plt.legend(loc = 'upper left')
   plt.grid(True)
   plt.savefig(var + 'compare.pdf', bbox_inches='tight')
   plt.close('all')
 
+def get_current_day():
+  current_month = calendar.month_name[datetime.now().month]
+  current_day = datetime.now().day
+  return current_month + ' ' + str(current_day)
+
 
 if __name__ == '__main__':
   state = 'Idaho'
-  df1 = get_state(pd.read_csv("../us-counties-5-9.csv", parse_dates=['date']), state)
-  df2 = get_state(pd.read_csv("../us-counties-5-11.csv", parse_dates=['date']), state)
-  df3 = get_state(pd.read_csv("../us-counties-may11.csv", parse_dates=['date']), state)
-  df1_name = 'May 9'
-  df2_name = 'May 11'
-  df3_name = 'May 11'
+  df1 = get_state(pd.read_csv("data/us-counties-may7.csv", parse_dates=['date']), state)
+  df2 = get_state(pd.read_csv("data/us-counties-may10.csv", parse_dates=['date']), state)
+  df3 = get_state(pd.read_csv("data/us-counties-latest.csv", parse_dates=['date']), state)
+  df1_name = 'May 7'
+  df2_name = 'May 10'
+  df3_name = get_current_day()
+
   #pd.set_option('display.max_rows', None)
 
   df1_ag = aggregate_df(df1)
--- raw_data_QA/get_data.sh
+++ raw_data_QA/get_data.sh
@@ -1,2 +1,6 @@
-curl -L https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv > us-counties.csv
-curl -L https://raw.githubusercontent.com/nytimes/covid-19-data/621fb652b003f049c1c3bc3985160da9db4fcf6d/us-counties.csv > us-counties-may8.csv
+rm -r data
+mkdir data
+curl -L https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv > data/us-counties-latest.csv
+curl -L https://raw.githubusercontent.com/nytimes/covid-19-data/1a710fb5ece55b8d5e36724353366b72387b1978/us-counties.csv > data/us-counties-may10.csv
+curl -L https://raw.githubusercontent.com/nytimes/covid-19-data/621fb652b003f049c1c3bc3985160da9db4fcf6d/us-counties.csv > data/us-counties-may8.csv
+curl -L https://raw.githubusercontent.com/nytimes/covid-19-data/b14a9b8dd14114cf2ecb4a943c7c5051ac94212a/us-counties.csv > data/us-counties-may7.csv
